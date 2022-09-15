51433468f2e25212caff41cbb9b65aac3ecd3fe8
Maksim <113623707+strix300@users.noreply.github.com>
2022-09-15 19:41:36 +0300

    vgtu
    vgtu

---
+++ korabl.c
@@ -0,0 +1,18 @@
+#include <locale.h>
+
+#include <stdio.h>
+
+void main()
+{
+	puts("         __         "); // 
+	puts("        /  |         "); // 
+	puts("       /   |         "); // 
+	puts("       \\   |         "); //
+	puts("        \\__|         "); // 
+	puts(" __________|__________"); // 
+	puts(" \\                   /"); // 
+	puts("  \\                 /"); // 
+	puts("   \\_______________/ "); // 
+	getchar(); //
+
+}
